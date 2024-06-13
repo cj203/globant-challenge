dedf1652ae6dc5369b93543fcb02c33f01806811
Carlos Rodriguez <carlosjose203@gmail.com>
2024-06-12 22:26:41 -0500

    update models, add database file

--- src/models.py
+++ src/models.py
@@ -1,22 +1,42 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class HiredEmployees(db.Model):
+class HiredEmployees(Base):
     __tablename__ = "hired_employees"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    hire_datetime = db.Column(db.Datetime, nullable=False)
-    department_id = db.Column(db.Integer, nullable=False)
-    job_id = db.Column(db.Integer, nullable=False)
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(255), nullable=False)
+    hire_datetime = Column(DateTime, nullable=False)
+    department_id = Column(Integer(), nullable=False)
+    job_id = Column(Integer(), nullable=False)
+
+    def __init__(self, name=None, 
+                 hire_datetime=None, 
+                 department_id=None, 
+                 job_id=None):
+        self.name = name
+        self.hire_datetime = hire_datetime
+        self.department_id = department_id
+        self.job_id = job_id
 
 
-class Departments(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    deparment_name = db.Column(db.String(100), nullable=False)
+class Departments(Base):
+    __tablename__ = "departments"
+    id = Column(Integer(), primary_key=True)
+    deparment_name = Column(String(100), nullable=False)
+
+    def __init__(self, deparment_name=None):
+        self.deparment_name = deparment_name
 
 
-class Jobs(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    job_name = db.Column(db.String(100), nullable=False)
+class Jobs(Base):
+    __tablename__ = "jobs"
+    id = Column(Integer(), primary_key=True)
+    job_name = Column(String(100), nullable=False)
+
+    def __init__(self, job_name=None):
+        self.job_name = job_name
+
+    def __repr__(self):
+        return f'<job_name {self.job_name!r}>'
