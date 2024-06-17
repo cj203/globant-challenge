96f66a527c2ad830427555eafbfc48f469f1e70d
Carlos Rodriguez <carlosjose203@gmail.com>
2024-06-17 15:40:24 -0500

    add data_sender.py file to test API

--- src/data_sender.py
+++ src/data_sender.py
@@ -1,25 +1,42 @@
 import requests
+import argparse
 import csv
 
 
-url = 'http://localhost:5000/hired_employees/v1/bulk'
 json_data = {
     'id': 1,
     'deparment_name':'Product Management',
 }
-files = {'file': open('src\data_challenge_files\hired_employees.csv', 'rb')}
 
-r = requests.post('http://localhost:5000/file/v1/hired_employees', files=files)
-print(r.text)
-# spamreader = csv.reader(open('src\data_challenge_files\hired_employees.csv', newline=''), delimiter=',')
-# sender_bulk = [row for row in spamreader]
-# print(sender_bulk[11:20])
-# # for row in sender_bulk:
-# #    print(row)
+def send_files(file="departments"):
+    files = {'file': open('src\data_challenge_files\{}.csv'.format(file), 'rb')}
 
-# response = requests.post(url, json=sender_bulk[11:20])
+    r = requests.post('http://localhost:5000/file/v1/{}'.format(file), files=files)
+    print(r.text)
 
-# if response.status_code == 200:
-#     print('JSON data sent successfully!')
-# else:
-#     print('Failed to send JSON data:', response.content)
+def send_bulk(file="departments"):
+    spamreader = csv.reader(open('src\data_challenge_files\{}.csv'.format(file), newline=''), delimiter=',')
+    sender_bulk = [row for row in spamreader]
+    url = 'http://localhost:5000/{}/v1/bulk'.format(file)
+    response = requests.post(url, json=sender_bulk)
+
+    if response.status_code == 200:
+        print('JSON data sent successfully!')
+    else:
+        print('Failed to send JSON data:', response.content)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", type=int, choices=[1, 2], required=True, help="Send bulk or file")
+    parser.add_argument("-f", type=str, choices=['departments', 'jobs', 'hired_employees'], help="the file name")
+
+    args = parser.parse_args()
+    print(f'Parametters {args}')
+    if args.t == 1:
+        send_bulk(args.f)
+    if args.t == 2:
+        if args.f:
+            send_files(args.f)
+        else:
+            send_files()
